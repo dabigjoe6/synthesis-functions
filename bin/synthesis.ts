cd3301746f840798ec119b657688eb7bca85c7e6
Joseph Olabisi <dabigjoe6@gmail.com>
2023-05-20 02:40:07 +0100

    Add env to Stack name

--- bin/synthesis.ts
+++ bin/synthesis.ts
@@ -2,6 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { SynthesisStack } from '../lib/synthesis-stack.js';
 
 const app = new cdk.App();
-new SynthesisStack(app, 'SynthesisStack', {
+new SynthesisStack(app, process.env.FUNCTION_NAME + 'Stack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
