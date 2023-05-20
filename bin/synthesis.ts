c9215e4230662062578b9b57bce4927dead40475
Joseph Olabisi <dabigjoe6@gmail.com>
2023-05-20 03:15:50 +0100

    Export stack name

--- bin/synthesis.ts
+++ bin/synthesis.ts
@@ -2,6 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { SynthesisStack } from '../lib/synthesis-stack.js';
 
 const app = new cdk.App();
-new SynthesisStack(app, (process.env.FUNCTION_NAME || "").replace(/(?:^|_)(\w)/g, (_, letter) => letter.toUpperCase()) + 'Stack', {
+new SynthesisStack(app, process.env.STACK_NAME || "SynthesisStack", {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
