a6427703ffd264c53bffb08e34de848b7e2fb0d7
Joseph Olabisi <dabigjoe6@gmail.com>
2023-05-20 02:53:43 +0100

    Fix stack name

--- bin/synthesis.ts
+++ bin/synthesis.ts
@@ -2,6 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { SynthesisStack } from '../lib/synthesis-stack.js';
 
 const app = new cdk.App();
-new SynthesisStack(app, process.env.FUNCTION_NAME + 'Stack', {
+new SynthesisStack(app, (process.env.FUNCTION_NAME || "").replace(/(?:^|_)(\w)/g, (_, letter) => letter.toUpperCase()) + 'Stack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
